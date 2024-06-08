4d978939867069b2e7730eea79fc52233b2cc673
Daniel alecode <danielalexisd8@gmail.com>
2024-06-08 10:35:13 -0600

    FrmConfiguracion

---
+++ BankManagement/src/Controller/ControllerPath.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+/**
+ *
+ * @author danie
+ */
+public class ControllerPath {
+    /*public static String PathName;
+    public static String Path;  
+    public static Class<E> PathContent;
+
+    public static String getPathName() {
+        return PathName;
+    }
+
+    public static void setPathName(String PathName) {
+        ControllerPath.PathName = PathName;
+    }
+
+    public static String getPath() {
+        return Path;
+    }
+
+    public static void setPath(String Path) {
+        ControllerPath.Path = Path;
+    }
+
+    public static Class getPathContent() {
+        return PathContent;
+    }
+
+    public static void setPathContent(Class PathContent) {
+        ControllerPath.PathContent = PathContent;
+    }
+    
+   
+    
+    public static boolean Serializar(){
+       return Model.Serializer.serializarObjeto(Path, PathName); 
+    }
+    
+    public static String Deserealizar(){
+        return  Model.Serializer.deserializarObjeto(PathName, PathContent); 
+    }*/
+}
---
+++ BankManagement/src/Model/Serializer.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+/**
+ *
+ * @author danie
+ */
+public class Serializer {
+     public static boolean serializarObjeto(String direccionArchivo, Serializable objeto) {
+        boolean sw = false;
+        try (FileOutputStream fos = new FileOutputStream(direccionArchivo);
+                ObjectOutputStream salida = new ObjectOutputStream(fos);) {
+            salida.writeObject(objeto);
+            sw = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return sw;
+    }
+
+    public static <E> E deserializarObjeto(String direccionArchivo, Class<E> claseObjetivo) {
+        E objeto = null;
+        try (FileInputStream fis = new FileInputStream(direccionArchivo);
+                ObjectInputStream entrada = new ObjectInputStream(fis);) {
+            objeto = (E) entrada.readObject();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return objeto;
+    }
+}
